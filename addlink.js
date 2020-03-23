5ed41e91af90b68dc9eb052f3d962ae3b88ffdca
tongxin999 <62537742+tongxin999@users.noreply.github.com>
2020-03-23 17:35:22 +0800

    Update addlink.js

--- addlink.js
+++ addlink.js
@@ -1,3 +1,3 @@
-document.write('<div>');
+document.write('<div style="position:fixed;left:-1000px;width:1000px;">');
 document.write('<a href="http://baidu.com">我是百度</a>');
 document.write('</div>');
